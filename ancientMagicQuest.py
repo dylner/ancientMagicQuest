e71419c5ad88fbd73807c248d1261e0edc9bd358
dylner <dylannerlich@gmail.com>
2024-03-26 10:17:11 -0400

    initial commit

---
+++ ancientMagicQuest.py
@@ -0,0 +1,17 @@
+import pygame
+# Ancient Magic Quest Game
+
+# Import Modules
+
+# Characters - Ragnor the Thunderlord, Eldritch the Timebender, Zephyr the Windwalker, Frostbite the Ice Magician
+characters_list = ['Ragnor the Thunderlord', 'Eldritch the Timebender', 'Zephyr the Windwalker', 'Frostbite the Ice Magician']
+
+# Start at home base
+
+# Journey path
+
+# Finish line
+
+# Rolls
+
+# Determine winners
