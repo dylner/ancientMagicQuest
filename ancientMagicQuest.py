7a66941255a225a27f0bcf78c5486961262ed7e4
elio-5 <165043132+elio-5@users.noreply.github.com>
2024-04-05 09:18:07 -0400

    Window

--- ancientMagicQuest.py
+++ ancientMagicQuest.py
@@ -2,7 +2,17 @@ import pygame
 # Ancient Magic Quest Game
 #Im HIMMMM!!
 # Import Modules
+import pygame
+pygame.init()
 
+screen=pygame.display.set_mode((1400,800))
+run = True
+while run:
+    pygame.time.delay(100)
+    for event in pygame .event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
 # Characters - Ragnor the Thunderlord, Eldritch the Timebender, Zephyr the Windwalker, Frostbite the Ice Magician
 characters_list = ['Ragnor the Thunderlord', 'Eldritch the Timebender', 'Zephyr the Windwalker', 'Frostbite the Ice Magician']
 
